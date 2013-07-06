9a82c3fe59be8223e0136b3655cd92ba476504c3
Arnab Deka <arnab.deka@gmail.com>
2013-07-06 10:30:32 +0530

    Use CyclicBarrier to make it happen

--- BarrierExample.java
+++ BarrierExample.java
@@ -1,8 +1,14 @@
+import java.util.concurrent.CyclicBarrier;
+
 public class BarrierExample {
 
   public static void main(String args[]){
-    for(int i=0; i<50; i++) {
-      new Thread(new Worker()).start();
+    int numThreads = 10;
+
+    final CyclicBarrier b = new CyclicBarrier(numThreads);
+
+    for(int i=0; i<numThreads; i++) {
+      new Thread(new Worker(b)).start();
     }
   }
 
--- Worker.java
+++ Worker.java
@@ -1,15 +1,29 @@
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
+
 public class Worker implements Runnable {
+
+    private CyclicBarrier barrier;
+    public Worker(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
 
     public void log(String message) {
         System.out.println(Thread.currentThread().getName() + ": " + message);
     }
 
-    public void run(){
-        log("running");
-        log("this should happen before reaching the barrier");
+    public void run() {
+        try {
+            log("running");
+            log("this should happen before reaching the barrier");
 
-        // TODO: insert barrier here
-        log("this should happen after the barrier");
+            barrier.await();
+            log("this should happen after the barrier");
+        } catch (InterruptedException ex) {
+            log("damn I hate interrupts! Committing suicide: " + ex);
+        } catch (BrokenBarrierException ex) {
+            log("damn, the dam burst: " + ex);
+        }
     }
 
 }
