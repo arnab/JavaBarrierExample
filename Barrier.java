0a7370ce348262482477aac82f5b89ae69945085
Arnab Deka <arnab.deka@gmail.com>
2013-07-06 10:49:40 +0530

    Implement a basic barrier

--- Barrier.java
+++ Barrier.java
@@ -1,9 +1,18 @@
 public class Barrier {
+  private final int numThreads;
+  private int waitingThreads;
+
   public Barrier(int numThreads) {
-    // TODO
+    this.numThreads = numThreads;
+    this.waitingThreads = 0;
   }
 
   public void await() {
-    // TODO
+    // TODO: can't just rely on the count, keep a list of threads waiting.
+    // Since the same thread may call await() more than once.
+    waitingThreads++;
+    while(waitingThreads < numThreads) {
+      Thread.currentThread().yield();
+    }
   }
 }
--- BarrierExample.java
+++ BarrierExample.java
@@ -3,10 +3,10 @@ public class BarrierExample {
   public static void main(String args[]){
     int numThreads = 10;
 
-    final Barrier b = new Barrier(numThreads);
+    final Barrier barrier = new Barrier(numThreads);
 
     for(int i=0; i<numThreads; i++) {
-      new Thread(new Worker(b)).start();
+      new Thread(new Worker(barrier)).start();
     }
   }
 
